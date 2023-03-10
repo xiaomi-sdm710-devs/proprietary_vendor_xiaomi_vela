2e728dfb8257bbab4a3b232b6996306b59d10327
me-cafebabe <me.cafebabe@gmail.com>
2023-03-11 00:10:52 +0800

    vela: Import stock camera blobs

--- vela-vendor.mk
+++ vela-vendor.mk
@@ -20,6 +20,11 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/vela/proprietary/vendor/etc/acdbdata/Forte/Forte_Speaker_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Forte/Forte_Speaker_cal.acdb \
     vendor/xiaomi/vela/proprietary/vendor/etc/acdbdata/Forte/Forte_workspaceFile.qwsp:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Forte/Forte_workspaceFile.qwsp \
     vendor/xiaomi/vela/proprietary/vendor/etc/acdbdata/adsp_avs_config.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/adsp_avs_config.acdb \
+    vendor/xiaomi/vela/proprietary/vendor/etc/camera/camxoverridesettings.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camxoverridesettings.txt \
+    vendor/xiaomi/vela/proprietary/vendor/etc/camera/golden_rear_dc_cal_wt.bin:$(TARGET_COPY_OUT_VENDOR)/etc/camera/golden_rear_dc_cal_wt.bin \
+    vendor/xiaomi/vela/proprietary/vendor/etc/camera/megviifacepp_0_5_2_model:$(TARGET_COPY_OUT_VENDOR)/etc/camera/megviifacepp_0_5_2_model \
+    vendor/xiaomi/vela/proprietary/vendor/etc/camera/org.quic.camera.debugdata.DebugDataAWB:$(TARGET_COPY_OUT_VENDOR)/etc/camera/org.quic.camera.debugdata.DebugDataAWB \
+    vendor/xiaomi/vela/proprietary/vendor/etc/camera/vidhance_calibration:$(TARGET_COPY_OUT_VENDOR)/etc/camera/vidhance_calibration \
     vendor/xiaomi/vela/proprietary/vendor/etc/dsi_ss_ea8076_fhd_cmd_display_f3m_mi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dsi_ss_ea8076_fhd_cmd_display_f3m_mi.xml \
     vendor/xiaomi/vela/proprietary/vendor/etc/hdr_config.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/hdr_config.cfg \
     vendor/xiaomi/vela/proprietary/vendor/etc/init/android.hardware.gatekeeper@1.0-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.gatekeeper@1.0-service-qti.rc \
@@ -73,21 +78,126 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/vela/proprietary/vendor/firmware/CAMERA_ICP.elf:$(TARGET_COPY_OUT_VENDOR)/firmware/CAMERA_ICP.elf \
     vendor/xiaomi/vela/proprietary/vendor/lib/android.hardware.camera.provider@2.4-external.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.camera.provider@2.4-external.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/android.hardware.camera.provider@2.4-legacy.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.camera.provider@2.4-legacy.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.sensor.pyxis_imx586.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.sensor.pyxis_imx586.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.sensor.pyxis_ov02a10.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.sensor.pyxis_ov02a10.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.sensor.pyxis_s5k4h7.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.sensor.pyxis_s5k4h7.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.sensor.pyxis_s5kgd1.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.sensor.pyxis_s5kgd1.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.sensormodule.pyxis_luxvisions_imx586_cn_i.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.sensormodule.pyxis_luxvisions_imx586_cn_i.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.sensormodule.pyxis_luxvisions_ov02a10_cn_i.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.sensormodule.pyxis_luxvisions_ov02a10_cn_i.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.sensormodule.pyxis_luxvisions_s5k4h7_cn_i.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.sensormodule.pyxis_luxvisions_s5k4h7_cn_i.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.sensormodule.pyxis_ofilm_ov02a10_cn_ii.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.sensormodule.pyxis_ofilm_ov02a10_cn_ii.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.sensormodule.pyxis_ofilm_s5k4h7_cn_ii.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.sensormodule.pyxis_ofilm_s5k4h7_cn_ii.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.sensormodule.pyxis_ofilm_s5kgd1_cn_ii.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.sensormodule.pyxis_ofilm_s5kgd1_cn_ii.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.sensormodule.pyxis_sunny_s5kgd1_cn_i.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.sensormodule.pyxis_sunny_s5kgd1_cn_i.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.tuned.default.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.tuned.default.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.tuned.pyxis_luxvisions_imx586_cn_i.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.tuned.pyxis_luxvisions_imx586_cn_i.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.tuned.pyxis_luxvisions_ov02a10_cn_i.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.tuned.pyxis_luxvisions_ov02a10_cn_i.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.tuned.pyxis_luxvisions_s5k4h7_cn_i.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.tuned.pyxis_luxvisions_s5k4h7_cn_i.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.tuned.pyxis_ofilm_ov02a10_cn_ii.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.tuned.pyxis_ofilm_ov02a10_cn_ii.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.tuned.pyxis_ofilm_s5k4h7_cn_ii.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.tuned.pyxis_ofilm_s5k4h7_cn_ii.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.tuned.pyxis_ofilm_s5kgd1_cn_ii.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.tuned.pyxis_ofilm_s5kgd1_cn_ii.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.tuned.pyxis_sunny_s5kgd1_cn_i.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.tuned.pyxis_sunny_s5kgd1_cn_i.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.meitu.node.mtBeauty.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.meitu.node.mtBeauty.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.meitu.node.offlinereproc.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.meitu.node.offlinereproc.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.eisv2.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.eisv2.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.eisv3.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.eisv3.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.hvx.addconstant.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.hvx.addconstant.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.hvx.binning.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.hvx.binning.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.node.dummybokeh.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.node.dummybokeh.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.node.dummyrtb.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.node.dummyrtb.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.node.dummysat.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.node.dummysat.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.node.eisv2.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.node.eisv2.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.node.eisv3.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.node.eisv3.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.node.gpu.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.node.gpu.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.node.memcpy.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.node.memcpy.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.node.mthdr.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.node.mthdr.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.node.mtldc.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.node.mtldc.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.node.mtsr.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.node.mtsr.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.node.remosaic.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.node.remosaic.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.node.singlebokeh.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.node.singlebokeh.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.node.stich.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.node.stich.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.node.swregistration.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.node.swregistration.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.stats.aec.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.stats.aec.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.stats.af.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.stats.af.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.stats.afd.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.stats.afd.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.stats.asd.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.stats.asd.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.stats.awb.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.stats.awb.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.stats.awbwrapper.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.stats.awbwrapper.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.stats.pdlib.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.stats.pdlib.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.stats.pdlibsony.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.stats.pdlibsony.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qti.stats.pdlibwrapper.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qti.stats.pdlibwrapper.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qtistatic.stats.aec.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qtistatic.stats.aec.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qtistatic.stats.af.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qtistatic.stats.af.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qtistatic.stats.awb.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qtistatic.stats.awb.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.qtistatic.stats.pdlib.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.qtistatic.stats.pdlib.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.vidhance.node.eis.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.vidhance.node.eis.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.vidhance.stats.aec_dmbr.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.vidhance.stats.aec_dmbr.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/com.visidon.node.sr.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/com.visidon.node.sr.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/components/libcam_mttouchgamma.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/components/libcam_mttouchgamma.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/fdconfigpreview.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/fdconfigpreview.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/fdconfigpreviewlite.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/fdconfigpreviewlite.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/fdconfigvideo.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/fdconfigvideo.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/fdconfigvideolite.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/fdconfigvideolite.bin \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera/titan17x_usecases.bin:$(TARGET_COPY_OUT_VENDOR)/lib/camera/titan17x_usecases.bin \
     vendor/xiaomi/vela/proprietary/vendor/lib/hw/android.hardware.camera.provider@2.4-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/android.hardware.camera.provider@2.4-impl.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/hw/audio.primary.sdm710.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/audio.primary.sdm710.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/hw/camera.qcom.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/camera.qcom.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/hw/com.qti.chi.override.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/com.qti.chi.override.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libBlendScreen.so:$(TARGET_COPY_OUT_VENDOR)/lib/libBlendScreen.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libGetExposureValue.so:$(TARGET_COPY_OUT_VENDOR)/lib/libGetExposureValue.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libHDRI.so:$(TARGET_COPY_OUT_VENDOR)/lib/libHDRI.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libMTChromaNR.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMTChromaNR.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libMTSkin.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMTSkin.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libManis.so:$(TARGET_COPY_OUT_VENDOR)/lib/libManis.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libMeituColorSystem.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMeituColorSystem.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libMeituColorSystem3DLut.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMeituColorSystem3DLut.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libMeituDeBlur.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMeituDeBlur.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libMeituDefocus.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMeituDefocus.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libPathBlur.so:$(TARGET_COPY_OUT_VENDOR)/lib/libPathBlur.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libVDSuperPhotoAPI.so:$(TARGET_COPY_OUT_VENDOR)/lib/libVDSuperPhotoAPI.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/liba2dpoffload.so:$(TARGET_COPY_OUT_VENDOR)/lib/liba2dpoffload.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/libadm.so:$(TARGET_COPY_OUT_VENDOR)/lib/libadm.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libalCFR.so:$(TARGET_COPY_OUT_VENDOR)/lib/libalCFR.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libalLDC.so:$(TARGET_COPY_OUT_VENDOR)/lib/libalLDC.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libalRnBRT_GL_GBWRAPPER.so:$(TARGET_COPY_OUT_VENDOR)/lib/libalRnBRT_GL_GBWRAPPER.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libalhLDC.so:$(TARGET_COPY_OUT_VENDOR)/lib/libalhLDC.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libarcsoft_dualcam_refocus.so:$(TARGET_COPY_OUT_VENDOR)/lib/libarcsoft_dualcam_refocus.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/libaudio_log_utils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libaudio_log_utils.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/libaudioroute_ext.so:$(TARGET_COPY_OUT_VENDOR)/lib/libaudioroute_ext.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libcam_meitu.so:$(TARGET_COPY_OUT_VENDOR)/lib/libcam_meitu.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libcamxfdalgov7.so:$(TARGET_COPY_OUT_VENDOR)/lib/libcamxfdalgov7.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libcamxfdengine.so:$(TARGET_COPY_OUT_VENDOR)/lib/libcamxfdengine.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libcamxstatscore.so:$(TARGET_COPY_OUT_VENDOR)/lib/libcamxstatscore.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libcom.qti.chinodeutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libcom.qti.chinodeutils.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libffmpeg.so:$(TARGET_COPY_OUT_VENDOR)/lib/libffmpeg.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libgnustl_shared.so:$(TARGET_COPY_OUT_VENDOR)/lib/libgnustl_shared.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/libhdmiedid.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhdmiedid.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/libhdmipassthru.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhdmipassthru.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/libhfp.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhfp.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libhidlcache.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhidlcache.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libmeitucapture.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmeitucapture.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libmibokeh_710.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmibokeh_710.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libmlabmakeup.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmlabmakeup.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libmpbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmpbase.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libmtCLUtil.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmtCLUtil.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libmtColorNoiseFilter.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmtColorNoiseFilter.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libmtMultiFrameDenoise.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmtMultiFrameDenoise.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libmtbodycontour.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmtbodycontour.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libmtface.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmtface.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libmtmakeupInterface.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmtmakeupInterface.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libmtnn.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmtnn.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libmtphotosegment.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmtphotosegment.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libmtvenom.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmtvenom.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/liborb.so:$(TARGET_COPY_OUT_VENDOR)/lib/liborb.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libremosaic_daemon.so:$(TARGET_COPY_OUT_VENDOR)/lib/libremosaic_daemon.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libremosaiclib.so:$(TARGET_COPY_OUT_VENDOR)/lib/libremosaiclib.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/libsensorslog.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsensorslog.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/libsndmonitor.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsndmonitor.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/libsnsapi.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsnsapi.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/libspkrprot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libspkrprot.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/libssc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libssc.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/libssrec.so:$(TARGET_COPY_OUT_VENDOR)/lib/libssrec.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/libvidhance.so:$(TARGET_COPY_OUT_VENDOR)/lib/libvidhance.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/rfsa/adsp/libVC1Dec.so:$(TARGET_COPY_OUT_VENDOR)/lib/rfsa/adsp/libVC1Dec.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/rfsa/adsp/libVC1DecDsp_skel.so:$(TARGET_COPY_OUT_VENDOR)/lib/rfsa/adsp/libVC1DecDsp_skel.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/rfsa/adsp/libVDHexagonSuperPhoto_skel.so:$(TARGET_COPY_OUT_VENDOR)/lib/rfsa/adsp/libVDHexagonSuperPhoto_skel.so \
