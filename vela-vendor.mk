44dc7150d9c362c9a1b99386d3171472d93e71bb
Yumi Yukimura <me.cafebabe@gmail.com>
2024-02-06 14:05:01 +0800

    vela: Squash of lineage-21 bringup commits
    commit 044524bb65e631a7bd962fee1adb92017c9a858f (HEAD -> lineage-21)
    Author: Yumi Yukimura <me.cafebabe@gmail.com>
    Date:   Tue Feb 6 13:59:49 2024 +0800
    
        vela: Patch blobs to load libstdc++_vendor.so instead
    
        Change-Id: Ib6195daf1791dca5d2c5461cb3389c944667ebba
    
    commit 5d01ff169390161fde1f8a7f9b84a043f7ca4df3
    Author: Yumi Yukimura <me.cafebabe@gmail.com>
    Date:   Tue Feb 6 13:56:21 2024 +0800
    
        vela: Use Camera provider blobs from vela
    
        Change-Id: Ie9604bc1ac2ccbcdb1befe75e0ceab2559e62dcb
    
    commit 37b30e7eb46dc7196ba4dad2337e5bdd4193d35f
    Author: Ivan Vecera <ivan@cera.cz>
    Date:   Wed Jan 31 15:49:32 2024 +0100
    
        vela: Switch to stock camera device libs
    
        Change-Id: I6f08d85c2e4652722ce2d6d0df0c74e469030506

--- vela-vendor.mk
+++ vela-vendor.mk
@@ -2034,6 +2034,11 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/vela/proprietary/vendor/firmware/CAMERA_ICP.elf:$(TARGET_COPY_OUT_VENDOR)/firmware/CAMERA_ICP.elf \
     vendor/xiaomi/vela/proprietary/vendor/lib/android.hardware.camera.provider@2.4-external.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.camera.provider@2.4-external.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/android.hardware.camera.provider@2.4-legacy.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.camera.provider@2.4-legacy.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera.device@1.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera.device@1.0-impl.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera.device@3.2-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera.device@3.2-impl.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera.device@3.3-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera.device@3.3-impl.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera.device@3.4-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera.device@3.4-impl.so \
+    vendor/xiaomi/vela/proprietary/vendor/lib/camera.device@3.5-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera.device@3.5-impl.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.sensor.pyxis_imx586.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.sensor.pyxis_imx586.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.sensor.pyxis_ov02a10.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.sensor.pyxis_ov02a10.so \
     vendor/xiaomi/vela/proprietary/vendor/lib/camera/com.qti.sensor.pyxis_s5k4h7.so:$(TARGET_COPY_OUT_VENDOR)/lib/camera/com.qti.sensor.pyxis_s5k4h7.so \
